5116ebbd34a69835fc63d817f5e927a2c7220b7c
Lucas <lvalencacomputacao@gmail.com>
2021-12-15 22:23:43 -0300

    Código já funciona, se for usado apenas o método 0 de ordenação

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import pygame, threading
+import pygame, threading, time
 
 import sorting
+
+def draw(phases, height, width, display):
+    for i in range(len(phases)):
+        k=1
+        time.sleep(1/len(phases[0])*k)
+        display.fill((0, 0, 0))
+        for j in range(len(phases[0])):
+            y = height-phases[i][j]
+            x = j * width/len(phases[0])
+            pygame.draw.circle(display, (255, 255, 255), (x, y), 5)
+        pygame.display.update()
 
 pygame.init()
 width = 800
@@ -8,18 +19,14 @@ height = 600
 gameDisplay = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Sort Visualizer")
 
-gameDisplay.fill((0, 0, 0))
-clock = pygame.time.Clock()
-running = True
-
 file = open("input.txt", "r")
 numbers = list(map(int, file.readlines()[0].split()))
 
 sorting_methods = [sorting.bubble_sort, sorting.insertion_sort, sorting.inversed_bubble_sort]
 
-def draw(phases):
-    print(phases[len(phases)-1])
-
+gameDisplay.fill((0, 0, 0))
+clock = pygame.time.Clock()
+running = True
 
 while running:
     for event in pygame.event.get():
@@ -29,9 +36,9 @@ while running:
                 if event.key-48 < len(sorting_methods) and event.key-48 >= 0:
                     sorting_method = sorting_methods[event.key-48]
                     phases = sorting_method(numbers)
-                    thread = threading.Thread(target=draw, args=(phases))
+
+                    thread = threading.Thread(target=draw, args=(phases, height, width, gameDisplay,))
                     thread.start()
-                    thread.join()
 
     pygame.display.update()
     clock.tick(60)
