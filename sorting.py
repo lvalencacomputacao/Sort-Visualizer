08ca9ce90d3297e619fd312f5848b5678eb44280
Lucas <lvalencacomputacao@gmail.com>
2021-12-15 23:02:05 -0300

    Código já funciona para organizar em ordem crescente ou decrescente

--- sorting.py
+++ sorting.py
@@ -11,5 +11,12 @@ def bubble_sort(vector):
 def insertion_sort():
     pass
 
-def inversed_bubble_sort():
-    pass
+def inversed_bubble_sort(vector):
+    to_return = []
+    for i in range(len(vector)):
+        for j in range(len(vector)-1-i):
+            if (vector[j] < vector[j+1]):
+                to_return.append(vector.copy())
+                vector[j], vector[j+1] = vector[j+1], vector[j]
+    to_return.append(vector.copy())
+    return to_return
